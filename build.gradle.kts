372935b9c7c175ed90aeffb6f4b6094a5abc617d
James Lyne <jim+github@not-null.co.uk>
2025-03-25 15:41:08 +0000

    Update gradle, plugins. Refactor gradle scripts
    - Switch to Kotlin
    - Switch to plugin-yml gradle plugin
    - Switch to using version catalog for dependencies
    - Use java toolchain

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+}
+
+group = "uk.co.notnull"
+version = "1.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://repo.not-null.co.uk/releases/")
+    }
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+dependencies {
+    compileOnly(libs.velocityApi)
+    compileOnly(libs.luckpermsApi)
+    compileOnly(libs.platformDetection)
+
+    annotationProcessor(libs.velocityApi)
+}
+
+tasks {
+    compileJava {
+        options.compilerArgs.addAll(listOf("-Xlint:all", "-Xlint:-processing"))
+        options.encoding = "UTF-8"
+    }
+
+    processResources {
+        expand("version" to project.version)
+    }
+}
